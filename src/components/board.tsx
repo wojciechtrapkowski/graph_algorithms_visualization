23d8529a508eab944d8f982bd2d439fd58d8421b
Wojtek <Wojtek@DESKTOP-4FI9M6M>
2023-09-06 21:04:02 +0200

    Implemented dragging objects on board

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -13,13 +13,33 @@ type BoardProps = {
 }
 
 export const Board = (props : BoardProps) => {
-    
+    let [dragMovement, setDragMovement] = useState([0, 0])
     const squareClasses ='w-6 h-6 border border-gray-300 m-0 flex justify-center';
 
     function onBoardClick(row : number, col : number) {
-        const newBoard = props.board.slice();
-        newBoard[row][col] = squareState.obstacle;
-        props.setBoard(newBoard);
+        if(props.board[row][col] == squareState.path) {
+            const newBoard = props.board.slice();
+            newBoard[row][col] = squareState.obstacle;
+            props.setBoard(newBoard);
+        }
+    }
+
+    function handleDragStart(e: React.DragEvent<HTMLDivElement>, x : number, y: number) {
+        let newDragMovement = [x, y];
+        setDragMovement(newDragMovement);
+    }
+
+    function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+    };
+
+    function handleDrop(e: React.DragEvent<HTMLDivElement>, x : number, y: number) {
+        if(props.board[x][y] === squareState.path) {
+            const newBoard = props.board.slice();
+            newBoard[x][y] = newBoard[dragMovement[0]][dragMovement[1]];
+            newBoard[dragMovement[0]][dragMovement[1]] = squareState.path;
+            props.setBoard(newBoard);
+        }
     }
 
     return (
@@ -28,13 +48,20 @@ export const Board = (props : BoardProps) => {
                 {props.board.map((row, rowIndex) => (
                 <div key={rowIndex}>
                     {row.map((cellValue, colIndex) => (
-                    <button
-                        key={colIndex}
-                        className={squareClasses}
-                        onClick={() => onBoardClick(rowIndex, colIndex)}
-                        style={props.board[rowIndex][colIndex] === squareState.visitedPath ? { backgroundColor: 'green' } : props.board[rowIndex][colIndex] === squareState.foundDestination ? {backgroundColor: 'blue'} : {} }>
-                        <Square fieldValue={cellValue} />
-                    </button>
+                    <div
+                    key={(rowIndex+1)*(colIndex+1)}
+                    draggable={true}
+                    onDragStart={(e) => handleDragStart(e, rowIndex, colIndex)}
+                    onDragOver={(e) => handleDragOver(e)}
+                    onDrop={(e) => handleDrop(e, rowIndex, colIndex)}>
+                        <button
+                            key={(colIndex+1)*( rowIndex+1)}
+                            className={squareClasses}
+                            onClick={() => onBoardClick(rowIndex, colIndex)}
+                            style={props.board[rowIndex][colIndex] === squareState.visitedPath ? { backgroundColor: 'green' } : props.board[rowIndex][colIndex] === squareState.foundDestination ? {backgroundColor: 'blue'} : {} }>
+                            <Square fieldValue={cellValue} />
+                        </button>
+                    </div>
                     ))}
                 </div>
                 ))}
